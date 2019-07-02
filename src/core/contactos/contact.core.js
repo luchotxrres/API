b563403da972484dc6206afcd8b9ce8e6a38d177
Nahuel Hernan Spiguelman <nahuel.spiguelman1@hotmail.com>
2019-07-01 23:03:17 -0300

    Maqueta server

--- src/core/contactos/contact.core.js
+++ src/core/contactos/contact.core.js
@@ -1,7 +1,18 @@
+const contactsServer = require('./../../db/server/contactos/contactos.server');
 module.exports = {
-    getAllcontactsCore: () => { },
-    getOneContactCore: (id) => { },
-    createContactCore: (body) => { },
-    updateContactCore: (body) => { },
-    deleteContactCore: (id) => { }
+    getAllcontactsCore: () => { 
+        return await contactsServer.getAllcontactsServer();
+    },
+    getOneContactCore: (id) => { 
+        return await contactsServer.getOneContactServer(id);
+    },
+    createContactCore: (body) => { 
+        return await contactsServer.createContactServer(body);
+    },
+    updateContactCore: (body) => { 
+        return await contactsServer.updateContactServer(body);
+    },
+    deleteContactCore: (id) => { 
+        return await contactsServer.deleteContactServer(id);
+    }
 }
---
+++ src/db/server/contactos/contactos.server.js
@@ -0,0 +1,7 @@
+module.exports = {
+    getAllcontactsServer: () => { },
+    getOneContactServer: (id) => { },
+    createContactServer: (body) => { },
+    updateContactServer: (body) => { },
+    deleteContactServer: (id) => { }
+}
