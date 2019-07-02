376201af088fe9df9f06c4c63b5062d004b943bf
Luciano Torres <ltorres@zentricx.com>
2019-07-01 22:02:02 -0300

    config .env

---
+++ src/config/env.config.js
@@ -0,0 +1,10 @@
+// config.js
+const dotenv = require('dotenv');
+var result = function() {
+    return dotenv.config({ path: '/.env' });
+}()
+
+if (result.error) {
+    throw result.error
+}
+module.exports.dotenv;
