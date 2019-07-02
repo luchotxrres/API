8b1802e6587eedd45d2059c15f289bfad34ee4a4
Nahuel Hernan Spiguelman <nahuel.spiguelman1@hotmail.com>
2019-07-02 20:26:20 -0300

    Fix test

--- src/test/index.js
+++ src/test/index.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const mongo_unit = require('mongo-unit');
 var data = require('./../helpers/init/initdb.json');
 mongoose.Promise = global.Promise;
-
-before(async () => {
-    mongoose.set('useCreateIndex', true);
-    await mongoose.connect('mongodb://localhost:27017/riesgo', { useNewUrlParser: true });
-    mongoose.set('useFindAndModify', false);
-    await mongo_unit.dropDb('mongodb://localhost:27017/riesgo');
-    return mongo_unit.initDb('mongodb://localhost:27017/riesgo', data);
-});
-
-before();
+describe('Inicio', () => {
+    before(async () => {
+        mongoose.set('useCreateIndex', true);
+        await mongoose.connect('mongodb://localhost:27017/riesgo', { useNewUrlParser: true });
+        mongoose.set('useFindAndModify', false);
+        await mongo_unit.dropDb('mongodb://localhost:27017/riesgo');
+        return mongo_unit.initDb('mongodb://localhost:27017/riesgo', data);
+    })
+    describe("Alta de contactos", () => {
+        it("asd", () => { })
+    });
+    describe("Consulta de contactos", () => {
+        it("asd", () => { })
+    });
+})
